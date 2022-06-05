3a2ff81b976d4aaa0edcb5d90ae88c56addffff9
Gergely Nagy <nagygr@gmail.com>
2022-06-06 00:00:08 +0200

    [ADD] Initial version of Rule and go fmt'ed.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"hash/maphash"
+	"log"
 	"strings"
 )
 
@@ -10,14 +12,15 @@ type RulerType int
 const (
 	CharacterType RulerType = iota
 	IfSuccessType
+	ReturnType
 )
 
 type Context struct {
-	Rules []int
-	Literals []string
-	RuleJumpTable map[string]int
-	FillTable []struct{
-		ruleName string
+	Rules         []int
+	Literals      []string
+	RuleJumpTable map[uint64]int
+	FillTable     []struct {
+		ruleHash uint64
 		position int
 	}
 }
@@ -40,7 +43,7 @@ type Character struct {
 var _ Ruler = (*Character)(nil)
 
 func NewCharacter(charSet string) *Character {
-	return &Character {
+	return &Character{
 		charSet: charSet,
 	}
 }
@@ -48,13 +51,13 @@ func NewCharacter(charSet string) *Character {
 func (c *Character) Build(ctx Context) Context {
 	ctx.Literals = append(ctx.Literals, c.charSet)
 	ctx.Rules = append(ctx.Rules, int(CharacterType))
-	ctx.Rules = append(ctx.Rules, len(ctx.Literals) - 1)
+	ctx.Rules = append(ctx.Rules, len(ctx.Literals)-1)
 
 	return ctx
 }
 
 type Concatenation struct {
-	leftRuler Ruler
+	leftRuler  Ruler
 	rightRuler Ruler
 }
 
@@ -62,20 +65,59 @@ var _ Ruler = (*Concatenation)(nil)
 
 func NewConcatenation(leftRuler Ruler, rightRuler Ruler) *Concatenation {
 	return &Concatenation{
-		leftRuler: leftRuler,
+		leftRuler:  leftRuler,
 		rightRuler: rightRuler,
 	}
 }
 
-func (c* Concatenation) Build(ctx Context) Context {
+func (c *Concatenation) Build(ctx Context) Context {
 	ctx = c.leftRuler.Build(ctx)
 	ctx.Rules = append(ctx.Rules, int(IfSuccessType))
 	truePos := len(ctx.Rules)
-	ctx.Rules = append(ctx.Rules, truePos + 2)
+	ctx.Rules = append(ctx.Rules, truePos+2)
 	ctx.Rules = append(ctx.Rules, 0)
 	ctx = c.rightRuler.Build(ctx)
 	afterRightPos := len(ctx.Rules)
-	ctx.Rules[truePos + 1] = afterRightPos
+	ctx.Rules[truePos+1] = afterRightPos
+
+	return ctx
+}
+
+type Rule struct {
+	InnerRule Ruler
+	hasher    maphash.Hash
+}
+
+var _ Ruler = (*Rule)(nil)
+
+func NewRule() *Rule {
+	return &Rule{
+		InnerRule: nil,
+	}
+}
+
+func (r *Rule) hash() uint64 {
+	r.hasher.Reset()
+	r.hasher.WriteString(
+		fmt.Sprintf("%v", r),
+	)
+	return r.hasher.Sum64()
+}
+
+func (r *Rule) Set(rule Ruler) {
+	r.InnerRule = rule
+}
+
+func (r *Rule) Build(ctx Context) Context {
+	if r == nil {
+		log.Panic("Uninitialized rule")
+	}
+
+	ruleStartPos := len(ctx.Rules)
+	ctx.RuleJumpTable[r.hash()] = ruleStartPos
+
+	ctx = r.InnerRule.Build(ctx)
+	ctx.Rules = append(ctx.Rules, int(ReturnType))
 
 	return ctx
 }
@@ -96,8 +138,8 @@ func (g *Grammar) Run(text string) bool {
 	for ruleIndex < len(g.Ctx.Rules) && textIndex < len(text) {
 		if RulerType(g.Ctx.Rules[ruleIndex]) == CharacterType {
 			var (
-				literalIndex = g.Ctx.Rules[ruleIndex + 1]
-				literal = g.Ctx.Literals[literalIndex]
+				literalIndex = g.Ctx.Rules[ruleIndex+1]
+				literal      = g.Ctx.Literals[literalIndex]
 			)
 
 			if strings.ContainsAny(string(text[textIndex]), literal) {
@@ -110,9 +152,9 @@ func (g *Grammar) Run(text string) bool {
 			}
 		} else if RulerType(g.Ctx.Rules[ruleIndex]) == IfSuccessType {
 			if ok {
-				ruleIndex = g.Ctx.Rules[ruleIndex + 1]
+				ruleIndex = g.Ctx.Rules[ruleIndex+1]
 			} else {
-				ruleIndex = g.Ctx.Rules[ruleIndex + 2]
+				ruleIndex = g.Ctx.Rules[ruleIndex+2]
 			}
 		}
 	}
@@ -166,7 +208,7 @@ func TestConcatenationOfConcatenation() bool {
 }
 
 func main() {
-	var tests = []func()bool{
+	var tests = []func() bool{
 		TestCharacter,
 		TestConcatenation,
 		TestConcatenationOfConcatenation,
